e4938ee4f15f9ec81bc9344f20f913dfcb815f92
dev4ox <dev4ox@gmail.com>
2023-04-14 09:13:25 +0000

    complete class Figure_checker complete screen out

--- main.py
+++ main.py
@@ -7,14 +7,39 @@ if __name__ == "__main__":
     #Здесь можно вызывать функции
     module.cheess_plate()
 
-    w_checker = module.Figure_checker(90, 'white')
-    b_checker = module.Figure_checker(90, 'black')
-    w_checker.draw((0, 0))
-    w_checker.draw((0, 100))
-    b_checker.draw((100, 0))
-    b_checker.draw((100, 100))
-    # module.draw_circle([150, 50], 50, [200, 0], 4, [200, 100, 0])
-    # module.Figure_checker()
+    w_ch_1 = module.Figure_checker('white', (0, 600), 90)
+    w_ch_2 = module.Figure_checker('white', (100, 500), 90)
+    w_ch_3 = module.Figure_checker('white', (100, 700), 90)
+    w_ch_4 = module.Figure_checker('white', (200, 600), 90)
+    w_ch_5 = module.Figure_checker('white', (300, 500), 90)
+    w_ch_6 = module.Figure_checker('white', (300, 700), 90)
+    w_ch_7 = module.Figure_checker('white', (400, 600), 90)
+    w_ch_8 = module.Figure_checker('white', (500, 500), 90)
+    w_ch_9 = module.Figure_checker('white', (500, 700), 90)
+    w_ch_10 = module.Figure_checker('white', (600, 600), 90)
+    w_ch_11 = module.Figure_checker('white', (700, 500), 90)
+    w_ch_12 = module.Figure_checker('white', (700, 700), 90)
+    w_checkers = [w_ch_1, w_ch_2, w_ch_3, w_ch_4, w_ch_5, w_ch_6,
+                  w_ch_7, w_ch_8, w_ch_9, w_ch_10, w_ch_11, w_ch_12]
+    b_ch_1 = module.Figure_checker('black', (0, 0), 90)
+    b_ch_2 = module.Figure_checker('black', (0, 200), 90)
+    b_ch_3 = module.Figure_checker('black', (100, 100), 90)
+    b_ch_4 = module.Figure_checker('black', (200, 0), 90)
+    b_ch_5 = module.Figure_checker('black', (200, 200), 90)
+    b_ch_6 = module.Figure_checker('black', (300, 100), 90)
+    b_ch_7 = module.Figure_checker('black', (400, 0), 90)
+    b_ch_8 = module.Figure_checker('black', (400, 200), 90)
+    b_ch_9 = module.Figure_checker('black', (500, 100), 90)
+    b_ch_10 = module.Figure_checker('black', (600, 0), 90)
+    b_ch_11 = module.Figure_checker('black', (600, 200), 90)
+    b_ch_12 = module.Figure_checker('black', (700, 100), 90)
+    b_checkers = [b_ch_1, b_ch_2, b_ch_3, b_ch_4, b_ch_5, b_ch_6,
+                  b_ch_7, b_ch_8, b_ch_9, b_ch_10, b_ch_11, b_ch_12]
+
+    for w_ch in w_checkers:
+        w_ch.draw()
+    for b_ch in b_checkers:
+        b_ch.draw()
 
 
     # Инициализация графического интерфейса
--- module.py
+++ module.py
@@ -45,39 +45,37 @@ def cheess_plate(num_rect=4, num_row=8):
             draw_rect([100, y_start_pos], [100, 100], [200, 0], num_rect)
             
 class Figure_checker:
-    def __init__(self, size=80, color='white'):
+    def __init__(self, color='white', pos=(0, 0), size=80):
         """
-        :type size: diameter
         :type color: 'white'/'black'
+        :param pos: base_position
+        :param size: diameter
         """
-        # self.center = pos[0] + (100 - size) // 2 + (size // 2),  pos[1] + (100 - size) // 2 + size // 2
+        self.center = pos[0] + (100 - size) // 2 + (size // 2),  pos[1] + (100 - size) // 2 + size // 2
         self.size = size
         self.radius = size // 2
         self.color = color
         self._w_color = (235, 235, 235)
-        self._b_color = (20, 20, 20)
-        self._g_color = (128, 128, 128)
-
-
-    def draw(self, pos=(0,0)):
+        self._r_color = (180, 10, 10)
+        self._g_color = (150, 150, 150)
+        self._b_color = (30, 30, 30)
+    def draw(self):
         """
-        :param pos: base_position
         :return: draw_figure
         """
-        center = pos[0] + (100 - self.size) // 2 + (self.size // 2), pos[1] + (100 - self.size) // 2 + self.size // 2
         if self.color == 'white':
-            pygame.draw.circle(w_setting.screen, self._g_color, center, self.radius)
-            pygame.draw.circle(w_setting.screen, self._w_color, center, self.radius-1)
-            pygame.draw.circle(w_setting.screen, self._b_color, center, self.radius-7)
-            pygame.draw.circle(w_setting.screen, self._w_color, center, self.radius-9)
-            pygame.draw.circle(w_setting.screen, self._b_color, center, self.radius-17)
-            pygame.draw.circle(w_setting.screen, self._w_color, center, self.radius-19)
+            pygame.draw.circle(w_setting.screen, self._b_color, self.center, self.radius)
+            pygame.draw.circle(w_setting.screen, self._w_color, self.center, self.radius-1)
+            pygame.draw.circle(w_setting.screen, self._g_color, self.center, self.radius-7)
+            pygame.draw.circle(w_setting.screen, self._w_color, self.center, self.radius-9)
+            pygame.draw.circle(w_setting.screen, self._g_color, self.center, self.radius-17)
+            pygame.draw.circle(w_setting.screen, self._w_color, self.center, self.radius-19)
         elif self.color == 'black':
-            pygame.draw.circle(w_setting.screen, self._g_color, center, self.radius)
-            pygame.draw.circle(w_setting.screen, self._b_color, center, self.radius-1)
-            pygame.draw.circle(w_setting.screen, self._w_color, center, self.radius-7)
-            pygame.draw.circle(w_setting.screen, self._b_color, center, self.radius-9)
-            pygame.draw.circle(w_setting.screen, self._w_color, center, self.radius-17)
-            pygame.draw.circle(w_setting.screen, self._b_color, center, self.radius-19)
+            pygame.draw.circle(w_setting.screen, self._b_color, self.center, self.radius)
+            pygame.draw.circle(w_setting.screen, self._r_color, self.center, self.radius-1)
+            pygame.draw.circle(w_setting.screen, self._b_color, self.center, self.radius-7)
+            pygame.draw.circle(w_setting.screen, self._r_color, self.center, self.radius-9)
+            pygame.draw.circle(w_setting.screen, self._b_color, self.center, self.radius-17)
+            pygame.draw.circle(w_setting.screen, self._r_color, self.center, self.radius-19)
         else:
             pass
